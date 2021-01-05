0f8bcadd7878822b01864be2f2a5c517e54b8e3b
Richard Williams <rkwilli20204@gmail.com>
2021-01-04 21:46:26 -0500

    Initial Misc. Files

---
+++ src/app/Test.java
@@ -0,0 +1,11 @@
+package app;
+
+import chessModel.ChessBoard;
+
+public class Test {
+
+	public static void main(String[] args) {
+		ChessBoard board = new ChessBoard();
+		System.out.println(board);
+	}
+}
